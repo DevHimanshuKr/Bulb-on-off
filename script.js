aca2a1b0252cc8468e3dbaaaecf0dccb722dd564
Himanshu Kumar <himanshu.6r0@gmail.com>
2025-02-07 12:31:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let oN = document.getElementById("on");
+let oF = document.getElementById("off");
+let bulb = document.getElementById("bulb-img");
+
+
+oN.addEventListener("click",function(){
+   bulb.src = "assest/bulb_on.gif";
+});  
+
+oF.addEventListener("click",function(){
+    bulb.src = "assest/bulb_off.gif";
+}) 
+
+
+
+// function bulbOn() {
+//     bulb.srcm = 
+// }
